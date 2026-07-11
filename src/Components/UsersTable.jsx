a8a6fc0f292e9fdb5e93d2e5b8ee66b8b64356f9
sabbirRashed <srsabbirrahman12@gmail.com>
2026-07-12 02:03:32 +0600

    create a table using backend data

---
+++ src/Components/UsersTable.jsx
@@ -0,0 +1,53 @@
+import { Button, Table } from '@heroui/react';
+import Link from 'next/link';
+import React from 'react';
+
+const UsersTable = ({ users }) => {
+    return (
+        <div className='mt-6'>
+            <Table>
+                <Table.ScrollContainer>
+                    <Table.Content aria-label="Team members" className="min-w-[600px]">
+                        <Table.Header>
+                            <Table.Column isRowHeader>Name</Table.Column>
+                            <Table.Column>Role</Table.Column>
+                            <Table.Column>Status</Table.Column>
+                            <Table.Column>Email</Table.Column>
+                            <Table.Column>Actions</Table.Column>
+                        </Table.Header>
+                        <Table.Body>
+                            {
+                                users.map(user => {
+                                    return (
+                                        <Table.Row key={user._id}>
+                                            <Table.Cell>{user.name}</Table.Cell>
+                                            <Table.Cell>{user.role}</Table.Cell>
+                                            <Table.Cell>Active</Table.Cell>
+                                            <Table.Cell>{user.email}</Table.Cell>
+                                            <Table.Cell className='space-x-2'>
+                                                <Link href={`/users/${user._id}`}>
+                                                    <Button variant="outline">Details</Button>
+                                                </Link>
+
+                                                <Link href={`/users/${user._id}`}>
+                                                    <Button variant="outline">Edit</Button>
+                                                </Link>
+
+                                                <Link href={`/users/${user._id}`}>
+                                                    <Button variant="danger">Delete</Button>
+                                                </Link>
+                                            </Table.Cell>
+                                        </Table.Row>
+                                    )
+                                })
+                            }
+
+                        </Table.Body>
+                    </Table.Content>
+                </Table.ScrollContainer>
+            </Table>
+        </div>
+    );
+};
+
+export default UsersTable;
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { getUsers } from '../lib/data';
+import UsersTable from '@/Components/UsersTable';
 
 const UsersPage = async() => {
     
     const users = await getUsers();
 
     return (
-        <div>
+        <div className='w-11/12 mx-auto p-6'>
             <h2>User Management: {users.length}</h2>
+            <UsersTable users={users}></UsersTable>
         </div>
     );
 };
